b0cb29d38013eedea315c2b6580f2ebe0b41d7df
EDM 1.15 <dev.edm115@gmail.com>
2022-05-19 16:44:15 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,20 @@
+from pyrogram import Client
+from pyromod import listen
+
+from config import Config
+
+SAMPLE = Client(
+        "SAMPLE",
+        bot_token=Config.BOT_TOKEN,
+        api_id=Config.APP_ID,
+        api_hash=Config.API_HASH,
+        sleep_threshold=10
+    )
+
+SAMPLE.start()
+
+@Client.on_message(filters.command("start"))
+async def start_bot(_, message: Message):
+    await message.reply_text(text=f"Hello {message.from_user.mention}")
+    
+SAMPLE.idle()
